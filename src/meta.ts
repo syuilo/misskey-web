e72790d49735ade6e94e4b07c96fafaed27d4f19
syuilo <syuilotan@yahoo.co.jp>
2016-10-04 19:53:17 +0900

    Update theme-color :art:

--- src/meta.ts
+++ src/meta.ts
@@ -5,5 +5,5 @@
 export const configDirPath = `${__dirname}/../.config`;
 export const configPath = `${configDirPath}/config.yml`;
 
-export const themeColor = '#fb6a5a';
+export const themeColor = '#f7796c';
 export const themeColorForeground = '#fff';
